2371662f0e87f5a4037e1de9f2f14b544c5b09c7
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-03-17 08:10:03 +0200

    navigate to organization dashboard component

--- src/components/Dashboard/NavigateToOrganization.tsx
+++ src/components/Dashboard/NavigateToOrganization.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { IOrganization } from "@/types"
-import { Button } from "../ui/button"
-import { SelectOrganization } from "./SelectOrganization"
-import CreateOrganizationModal from "./createOrgModal"
-import { useState } from "react"
-import { useRouter } from 'next/navigation';
+import { IOrganization } from '@/types'
+import { Button } from '../ui/button'
+import { SelectOrganization } from './SelectOrganization'
+import CreateOrganizationModal from './createOrgModal'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
-export function NavigateToOrganization({organizations}: {organizations: IOrganization[]}) {
+export function NavigateToOrganization({ organizations }: { organizations: IOrganization[] }) {
   const [selected, setSelected] = useState<IOrganization>(organizations[0])
   const router = useRouter()
   function onSelect(org: IOrganization) {
@@ -20,7 +20,9 @@ export function NavigateToOrganization({organizations}: {organizations: IOrganiz
     <>
       <SelectOrganization organizations={organizations} onSelect={onSelect} />
       <CreateOrganizationModal />
-      <Button variant={'destructive'} onClick={onClick}>Submit</Button>
+      <Button variant={'destructive'} onClick={onClick}>
+        Submit
+      </Button>
     </>
   )
 }
