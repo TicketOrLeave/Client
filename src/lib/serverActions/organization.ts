c1ac997f0485ea5bbf4e3d93555989e9ac0cd43b
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-03-17 07:48:05 +0200

    get and post organization server actions

--- src/lib/serverActions/organization.ts
+++ src/lib/serverActions/organization.ts
@@ -4,7 +4,16 @@ import fetcher from '../fetcher'
 
 export async function createOrganization(name: string): Promise<APIRespone<IOrganization>> {
   try {
-    const res = await fetcher.post(`/organization/?name=${name}`)
+    const res = await fetcher.post(`/organizations/?name=${name}`)
+    return { success: true, data: res.data }
+  } catch (error) {
+    return { success: false, error: error as APIError }
+  }
+}
+
+export async function getOrganizations(): Promise<APIRespone<IOrganization[]>> {
+  try {
+    const res = await fetcher.get(`/organizations/`)
     return { success: true, data: res.data }
   } catch (error) {
     return { success: false, error: error as APIError }
