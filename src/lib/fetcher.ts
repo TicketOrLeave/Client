d6e05da4c5d6541a407df6105ffca8ac4911f207
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-03-17 07:46:56 +0200

    refactor fetcher instance to include cookies on each request - work around to fix nvariant: cookies() expects to have requestAsyncStorage bug in dynamic routing nextjs

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,7 +1,24 @@
 import axios from 'axios'
 import { cookies } from 'next/headers'
 
-export default axios.create({
-  baseURL: process.env.SERVER_API_URL,
-  headers: { Cookie: cookies().toString() },
-})
+async function fetcher() {
+  const fetcher = axios.create({
+    baseURL: process.env.SERVER_API_URL,
+  })
+  return {
+    get: (url: string, config: any = {}) => {
+      return fetcher.get(url, { ...config, headers: { Cookie: cookies().toString() } })
+    },
+    post: (url: string, data?: any, config: any = {}) => {
+      return fetcher.post(url, data, { ...config, headers: { Cookie: cookies().toString() } })
+    },
+    put: (url: string, data?: any, config: any = {}) => {
+      return fetcher.put(url, data, { ...config, headers: { Cookie: cookies().toString() } })
+    },
+    delete: (url: string, config: any = {}) => {
+      return fetcher.delete(url, { ...config, headers: { Cookie: cookies().toString() } })
+    },
+  }
+}
+
+export default await fetcher()
