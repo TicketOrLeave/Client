720000f7ef430237b1e3e8b4044d57d7235526c0
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-03-18 14:17:04 +0200

    fix dashboard responsive

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ export default async function Page() {
   const organizations = await getOrganizations()
 
   return (
-    <div className="flex flex-col gap-4 justify-center items-center min-h-screen">
+    <div className="flex flex-col gap-4 justify-center items-center min-h-full">
       <h1>Select an organization or Create a new one: </h1>
       {organizations.success ? <NavigateToOrganization organizations={organizations.data} /> : null}
     </div>
