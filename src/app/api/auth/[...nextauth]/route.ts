754ac905c703fc2ac2cf6dd935568f5c5fc2f2c1
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-03-28 15:42:48 +0200

    fix: export not nedded authOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,11 @@
-import type { NextAuthOptions, User } from 'next-auth'
+import type { NextAuthOptions } from 'next-auth'
 import NextAuth from 'next-auth/next'
 import GooglePorvider from 'next-auth/providers/google'
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt',
   },
