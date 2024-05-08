7c74c60035a56f273faf6de2ff1fed6d21fa98ec
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-05-08 22:29:56 +0300

    update photo width and heigth after selecting it

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -27,13 +27,13 @@ export function FileUploader({ imageUrl, onFieldChange, setFiles }: FileUploader
   return (
     <div
       {...getRootProps()}
-      className="flex-center bg-dark-3 flex border p-2 cursor-pointer flex-col overflow-hidden rounded-md bg-grey-50"
+      className="flex-center items-center bg-dark-3 flex border p-2 cursor-pointer flex-col overflow-hidden rounded-md bg-grey-50"
     >
       <input {...getInputProps()} className="cursor-pointer" />
 
       {imageUrl ? (
-        <div className="flex h-full w-full flex-1 justify-center ">
-          <Image src={imageUrl} alt="image" width={250} height={250} className="w-full object-cover object-center" />
+        <div className="flex h-[100px] w-[100px] flex-1 justify-center">
+          <Image src={imageUrl} alt="image" width={50} height={50} className="w-full object-cover object-center" />
         </div>
       ) : (
         <div className="flex-center flex-col py-5 text-grey-500 justify-center items-center flex">
